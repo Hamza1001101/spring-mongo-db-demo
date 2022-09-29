c30f34615254129a7b5fcbde3f5180ef38e6dde6
Hamza Hayd <dado.org@hotmail.com>
2022-09-30 01:52:03 +0200

    first commit

---
+++ src/main/java/com/example/mongodbdemo/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.mongodbdemo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface EmployeeRepository extends MongoRepository<Employee, Long> {
+}
